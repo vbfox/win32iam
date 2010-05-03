f5e18f75c3a67ea4793201c5fcb1b7a3d9fcc1d6
Julien Roncaglia <julien@roncaglia.fr>
2010-05-03 12:17:34 +0000

    Moved to VS2010

--- InstalledApplicationsManager/Properties/Settings.Designer.cs
+++ InstalledApplicationsManager/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace VisualUninstaller.Properties {
+namespace BlackFox.InstalledApplicationsManager.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
